4a333515daac432c8e7dc6764e956f44c970bb6c
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2020-07-08 17:53:32 +0300

    Added simple example

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import { createPow } from "@textile/powergate-client";
+import fs from "fs"
+import { createPow, ffsTypes } from "@textile/powergate-client";
 const host = "http://0.0.0.0:6002"; // local running powergate instance
 const pow = createPow({ host });
 
 (async function () {
-    const pow = createPow({ host });
     const { status, messageList } = await pow.health.check();
     const { peersList } = await pow.net.peers();
     const { token } = await pow.ffs.create();
@@ -11,5 +11,24 @@ const pow = createPow({ host });
     // get wallet addresses associated with your FFS instance
     const { addrsList } = await pow.ffs.addrs();
 
-    console.log(addrsList);
+    const filename = 'rick.jpg';
+
+    const buffer = fs.readFileSync(`input/${filename}`);
+    const { cid } = await pow.ffs.addToHot(buffer);
+
+
+    // store the data in FFS using the default storage configuration
+    const { jobId } = await pow.ffs.pushConfig(cid);
+
+    // watch all FFS events for a cid
+    pow.ffs.watchLogs((logEvent) => {
+        console.log(`received event for cid ${logEvent.cid}`);
+        console.log(logEvent);
+    }, cid);
+
+    // get the current actual storage configuration for a cid
+    const arrayBuffer = await pow.ffs.get(cid);
+    fs.mkdirSync('output');
+    fs.writeFileSync(`output/${filename}`, new Buffer(arrayBuffer));
 })();
+
